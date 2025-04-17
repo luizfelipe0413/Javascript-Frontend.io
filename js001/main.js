c295658e6fa1a38969c6afaf11a15757e9076e5d
Strawberry Luiz <luizkadu20@gmail.com>
2025-04-17 20:29:47 -0300

    progresso

---
+++ js001/main.js
@@ -0,0 +1,6 @@
+var sharebut = document.getElementById  ('sharebutton')
+var sharepop = document.getElementById('popup')
+function popup(){
+    sharepop.classList.toggle('show');
+    sharebut.classList.toggle('active');
+};
